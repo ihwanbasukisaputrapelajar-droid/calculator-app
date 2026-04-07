3cde1ba6698d63e0790fb8cce78a3a870bb7232d
ihwanbasukisaputrapelajar <ihwanbasukisaputrapelajar@gmail.com>
2026-04-07 15:05:03 +0700

    variabel dan method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int var1 = 10;
+int var2 = 6;
+
+static int penjumlahan(int var1, int var2)
+{
+    return var1 + var2;
+}
+static int penguragan(int var1, int var2)
+{
+    return var1 + var2;
+}
+static int perkalian(int var1, int var2)
+{
+    return (var1 + var2) * var2;
+}
+static int pembagian(int var1, int var2)
+{
+    return var1 + var2;
+}

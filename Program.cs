b75bc700523a7eb87190d8b78d526fadcb5bad76
ihwanbasukisaputrapelajar-droid <ihwanbasukisaputrapelajar@gmail.com>
2026-04-07 23:19:39 +0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -5,15 +5,23 @@ static int penjumlahan(int var1, int var2)
 {
     return var1 + var2;
 }
-static int penguragan(int var1, int var2)
+static int pengurangan(int var1, int var2)
 {
-    return var1 + var2;
+    return var1 - var2;
 }
 static int perkalian(int var1, int var2)
 {
-    return (var1 + var2) * var2;
+    return (var1 * var2);
 }
 static int pembagian(int var1, int var2)
 {
-    return var1 + var2;
+    return var1 / var2;
 }
+    Console.WriteLine("Penjumlahan: " + var1 + " + " + var2 + " = " + penjumlahan(var1, var2));
+    Console.WriteLine("Pengurangan: " + var1 + " - " + var2 + " = " + pengurangan(var1, var2));
+    Console.WriteLine("Perkalian: " + var1 + " * " + var2 + " = " + perkalian(var1, var2));
+    Console.WriteLine("Pembagian: " + var1 + " / " + var2 + " = " + pembagian(var1, var2));
+    Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+    Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+
+  
